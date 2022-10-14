cc60490e4b7f073694d6a500f085060235d2d8e2
Yudhapermanaa <203040101@mail.unpas.ac.id>
2022-10-14 15:05:38 +0700

    membuat database wrapper

--- mvc/app/core/Constant.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_b_203040101/mvc/public');
--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -2,19 +2,25 @@
 
 class Mahasiswa_model
 {
-    private $dbh; // databse handler
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=prakweb_2022_b_203040101';
+        $this->db = new Database;
+    }
 
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+    public function getAllMahasiswa()
+    {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . 'WHERE id =:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 
     // private $mhs = [
@@ -37,12 +43,4 @@ class Mahasiswa_model
     //         "jurusan" => "Teknik Informatika"
     //     ]
     // ];
-
-
-    public function getAllMahasiswa()
-    {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
 }
